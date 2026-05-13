3b4c32f2e9f84f766d07b332886817f7f97f4a49
MuhammadAnasRasheed <rasheedanas2000@gmail.com>
2026-05-13 15:32:35 +0500

    feat: Update user routes in main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import invoiceRoutes from './invoiceRoutes';
 import customerRoutes from './customerRoutes';
+import userRoutes from '../routes/userRoutes'
 
 const router = Router();
 
 router.use('/invoices', invoiceRoutes);
-router.use('/customers', customerRoutes); // Authentication routes
-
+router.use('/customers', customerRoutes); 
+router.use('/users', userRoutes);
 export default router;
